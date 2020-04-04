7c287d476f734e5c2527f556de46dae1238060a4
wilantury <ventas@antury.net>
2020-04-04 09:13:19 -0500

    [ADD] slide compoente

---
+++ src/components/SlideHands.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../assets/styles/components/SlideHands.css'
+import stepClean from '../assets/static/stepOne.png';
+
+
+const SlideHands = () => {
+    return(
+      <section className="slide-container">
+        <div className="slide-container__text">
+          <p>Paso 1</p>
+        </div>
+        <div className="slide-container__image">
+          <img src={stepClean} alt="paso 2"/>
+        </div>
+      </section>
+    );
+}
+
+export default SlideHands;
